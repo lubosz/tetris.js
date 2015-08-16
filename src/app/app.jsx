625f2cb37c212d54eb1d35005d2a761481f31af2
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2015-08-16 17:42:17 +0200

    move tetris piece functions to tetris.jsx

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,5 @@
 //import {keydown, handleGamePadAction} from './inputhandle.jsx'
-
+import {randomTetrisPiece} from './tetris.jsx'
 
 
 var GeneralKEYs = 
@@ -216,7 +216,7 @@ function keydown(ev)
 //-------------------------------------------------------------------------
 var playing = false;
 //set to "puyp" or "tetris"
-var mode = "puyu";
+var mode = "tetris";
 var nx = 10;    // width of tetris court (in blocks)
 var ny = 20;    // height of tetris court (in blocks)
 var nu = 5;     // width/height of upcoming preview (in blocks)
@@ -273,15 +273,7 @@ function timestamp()
     return new Date().getTime();
 }
 
-function random(min, max) 
-{
-    return (min + (Math.random() * (max - min)));
-}
 
-function randomChoice(choices) 
-{
-    return choices[Math.round(random(0, choices.length - 1))];
-}
 
 if (!window.requestAnimationFrame) 
 {
@@ -296,87 +288,29 @@ if (!window.requestAnimationFrame)
     }
 }
 
-//-------------------------------------------------------------------------
-// tetris pieces
-//
-// blocks: each element represents a rotation of the piece (0, 90, 180, 270)
-//       each element is a 16 bit integer where the 16 bits represent
-//       a 4x4 set of blocks, e.g. j.blocks[0] = 0x44C0
-//
-//           0100 = 0x4 << 3 = 0x4000
-//           0100 = 0x4 << 2 = 0x0400
-//           1100 = 0xC << 1 = 0x00C0
-//           0000 = 0x0 << 0 = 0x0000
-//                             ------
-//                             0x44C0
-//
-//-------------------------------------------------------------------------
-var i = 
-{
-    size: 4,
-    blocks: [0x0F00, 0x2222, 0x00F0, 0x4444],
-    color: 'cyan'
-};
-var j = 
-{
-    size: 3,
-    blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20],
-    color: 'blue'
-};
-var l = 
-{
-    size: 3,
-    blocks: [0x4460, 0x0E80, 0xC440, 0x2E00],
-    color: 'orange'
-};
-var o = 
-{
-    size: 2,
-    blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00],
-    color: 'yellow'
-};
-var s = 
-{
-    size: 3,
-    blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620],
-    color: 'green'
-}; 
-var t = 
-{
-    size: 3,
-    blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640],
-    color: 'purple'
-};
-var z = 
-{
-    size: 3,
-    blocks: [0x0C60, 0x4C80, 0xC600, 0x2640],
-    color: 'red'
-};
 
 function getRandomPuyuColor()
 {
     return puyucolors[Math.floor(Math.random() * puyucolors.length)];
 }
 
+function randomPuyoPiece() {
+    return ({
+        size: 2,
+        blocks: [0x0C00, 0x4400, 0x0C00, 0x4400],
+        color: getRandomPuyuColor(), 
+        color2: getRandomPuyuColor()
+    });
+}
+
+
 function randomPiece() 
 {
     var type;
     if(mode == "puyu")
-    { 
-        type = 
-        {
-            size: 2,
-            blocks: [0x0C00, 0x4400, 0x0C00, 0x4400],
-            color: getRandomPuyuColor(), 
-            color2: getRandomPuyuColor()
-        }
-    }
+        type = randomPuyoPiece();
     else if(mode == "tetris")
-    {
-        var pieces = [i,i,i,i,j,j,j,j,l,l,l,l,o,o,o,o,s,s,s,s,t,t,t,t,z,z,z,z];
-        type = pieces.splice(random(0, pieces.length-1), 1)[0];
-    }
+        type = randomTetrisPiece();
     return {type: type, dir: DIR.UP, x: Math.round(nx / 2 - 2), y: -2};
 }
 
---
+++ src/app/tetris.jsx
@@ -0,0 +1,72 @@
+//-------------------------------------------------------------------------
+// tetris pieces
+//
+// blocks: each element represents a rotation of the piece (0, 90, 180, 270)
+//       each element is a 16 bit integer where the 16 bits represent
+//       a 4x4 set of blocks, e.g. j.blocks[0] = 0x44C0
+//
+//           0100 = 0x4 << 3 = 0x4000
+//           0100 = 0x4 << 2 = 0x0400
+//           1100 = 0xC << 1 = 0x00C0
+//           0000 = 0x0 << 0 = 0x0000
+//                             ------
+//                             0x44C0
+//
+//-------------------------------------------------------------------------
+var i = 
+{
+    size: 4,
+    blocks: [0x0F00, 0x2222, 0x00F0, 0x4444],
+    color: 'cyan'
+};
+var j = 
+{
+    size: 3,
+    blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20],
+    color: 'blue'
+};
+var l = 
+{
+    size: 3,
+    blocks: [0x4460, 0x0E80, 0xC440, 0x2E00],
+    color: 'orange'
+};
+var o = 
+{
+    size: 2,
+    blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00],
+    color: 'yellow'
+};
+var s = 
+{
+    size: 3,
+    blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620],
+    color: 'green'
+}; 
+var t = 
+{
+    size: 3,
+    blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640],
+    color: 'purple'
+};
+var z = 
+{
+    size: 3,
+    blocks: [0x0C60, 0x4C80, 0xC600, 0x2640],
+    color: 'red'
+};
+
+function random(min, max) 
+{
+    return (min + (Math.random() * (max - min)));
+}
+
+function randomChoice(choices) 
+{
+    return choices[Math.round(random(0, choices.length - 1))];
+}
+
+export function randomTetrisPiece() {
+    let pieces = [i,i,i,i,j,j,j,j,l,l,l,l,o,o,o,o,s,s,s,s,t,t,t,t,z,z,z,z];
+    return pieces.splice(random(0, pieces.length-1), 1)[0];
+}
