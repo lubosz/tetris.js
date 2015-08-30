65edc139fcfa5f01a43faf9f94d0da18c6a5c00d
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2015-08-31 00:10:42 +0200

    split into multiple components

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,74 +1,87 @@
 import React from 'react';
 
-class Main extends React.Component {
 
+class Player extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
+        let pl = "player" + this.props.number;
+        let hold = "holdP" + this.props.number;
+        let avatar = "avatarP" + this.props.number;
+        let name = "nameP" + this.props.number;
+        let score = "scoreP" + this.props.number;
+        let rows = "rowsP" + this.props.number;
+        let wins = "winsP" + this.props.number;
+        let end = "endP" + this.props.number;
+        let canvas = "canvasP" + this.props.number;
+        let upcoming = "upcomingP" + this.props.number;
 
-        let hiddenStyle = {
-            visibility: 'hidden'
-        };
-
-        let absoluteStyle = {
-            position: 'absolute'
+        let avatarStyle = {
+            background: "url(img/lubosz.jpg)",
+            backgroundSize: "contain",
+            height: "15vh",
         };
 
         return (
-<div id="outer">
-    <div id="inner">
-        <div id="messages" style={absoluteStyle}>
-            <div id="start" className="message">Press X to Play</div>
-            <div id="paused" className="message" style={hiddenStyle}>Paused</div>
-        </div>
-
-        <div id="player1">
-            <div className="hud">
-                <canvas id="holdP1"></canvas>
-                <div id="avatarP1"></div>
-                <div>
-                    <span id="nameP1">Lubosz</span><br/>
-                    Score: <span id="scoreP1">00000</span><br/>
-                    Rows: <span id="rowsP1">0</span><br/>
-                    Total wins: <span id="winsP1">0</span><br/>
+            <div id={pl}>
+                <div className="hud">
+                    <canvas id={hold}></canvas>
+                    <div style={avatarStyle}></div>
+                    <div>
+                        <span id={name}>{this.props.name}</span><br/>
+                        Score: <span id={score}>00000</span><br/>
+                        Rows: <span id={rows}>0</span><br/>
+                        Total wins: <span id={wins}>0</span><br/>
+                    </div>
+                    <div id={end}></div>
                 </div>
-                <div id="endP1"></div>
-            </div>
-            <canvas id="canvasP1">
-                Sorry, this game cannot be run because your browser does not support the &lt;canvas&gt; element
-            </canvas>
-            <div className="hud">
-                <canvas id="upcomingP1"></canvas>
-            </div>
-        </div>
-
-        <div id="player2">
-            <div className="hud">
-                <canvas id="holdP2"></canvas>
-                <div id="avatarP2"></div>
-                <div>
-                    <span id="nameP2">Jessi</span><br/>
-                    Score: <span id="scoreP2">00000</span><br/>
-                    Rows: <span id="rowsP2">0</span><br/>
-                    Total wins: <span id="winsP2">0</span><br/>
+                <canvas id={canvas}>
+                    Sorry, this game cannot be run because your browser does not support the &lt;canvas&gt; element
+                </canvas>
+                <div className="hud">
+                    <canvas id={upcoming}></canvas>
                 </div>
-                <div id="endP2"></div>
             </div>
-            <canvas id="canvasP2">
-                Sorry, this game cannot be run because your browser does not support the &lt;canvas&gt; element
-            </canvas>
-            <div className="hud">
-                <canvas id="upcomingP2"></canvas>
-            </div>
-        </div>
-    </div>
-</div>
-
         );
     }
+}
 
+class Messages extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let hiddenStyle = {
+            visibility: 'hidden'
+        };
+        let absoluteStyle = {
+            position: 'absolute'
+        };
+        return (
+            <div id="messages" style={absoluteStyle}>
+                <div id="start" className="message">Press X to Play</div>
+                <div id="paused" className="message" style={hiddenStyle}>Paused</div>
+            </div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div id="outer">
+                <div id="inner">
+                    <Messages />
+                    <Player number="1" name="Lubosz" />
+                    <Player number="2" name="Jessi" />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Main;
