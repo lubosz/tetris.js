30234bca12d08a586297795a2523f32867e7c97e
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2015-08-30 22:48:05 +0200

    use cooler block colors

--- src/app/tetris.js
+++ src/app/tetris.js
@@ -17,43 +17,43 @@ var i =
 {
     size: 4,
     blocks: [0x0F00, 0x2222, 0x00F0, 0x4444],
-    color: 'cyan'
+    color: '#20c8f8' // cyan
 };
 var j =
 {
     size: 3,
     blocks: [0x44C0, 0x8E00, 0x6440, 0x0E20],
-    color: 'blue'
+    color: '#6060f8' // blue
 };
 var l =
 {
     size: 3,
     blocks: [0x4460, 0x0E80, 0xC440, 0x2E00],
-    color: 'orange'
+    color: '#f87820' // orange
 };
 var o =
 {
     size: 2,
     blocks: [0xCC00, 0xCC00, 0xCC00, 0xCC00],
-    color: 'yellow'
+    color: '#f8d000' // yellow
 };
 var s =
 {
     size: 3,
     blocks: [0x06C0, 0x8C40, 0x6C00, 0x4620],
-    color: 'green'
+    color: '#00d000' // green
 };
 var t =
 {
     size: 3,
     blocks: [0x0E40, 0x4C40, 0x4E00, 0x4640],
-    color: 'purple'
+    color: '#d828d8' // purple / pink
 };
 var z =
 {
     size: 3,
     blocks: [0x0C60, 0x4C80, 0xC600, 0x2640],
-    color: 'red'
+    color: '#f01818' // red
 };
 
 function random(min, max) {
