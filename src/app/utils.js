9baa4fc33a56078441a0b29e8ebec7e880fc2913
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2015-08-31 15:22:47 +0200

    remove obsolete code

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,22 +1,3 @@
-//-------------------------------------------------------------------------
-// base helper methods
-//-------------------------------------------------------------------------
-export function get(id) {
-    return document.getElementById(id);
-}
-
-export function html(id, html) {
-    get(id).innerHTML = html;
-}
-
-export function show(id) {
-    get(id).style.visibility = 'visible';
-}
-
-export function hide(id) {
-    get(id).style.visibility = 'hidden';
-}
-
 export function timestamp() {
     return new Date().getTime();
 }
