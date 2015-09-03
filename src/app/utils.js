e6e208feb523a48b79e866a9ef5ed90180e205e7
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2015-09-03 22:37:57 +0200

    disable music

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,13 +2,13 @@ export function timestamp() {
     return new Date().getTime();
 }
 
-let music;
+//let music;
 
 export function sound(name) {
     const sound = new Audio('sound/' + name + '.ogg');
     sound.play();
 }
 export function music(name) {
-    music = new Audio('music/' + name + '.ogg');
-    music.play();
+    let gamemusic = new Audio('music/' + name + '.ogg');
+    //music.play();
 }
